6e2c355c30bfa40e1fc0267d457e5f5fa21c5506
Marcin <marcin4951@gmail.com>
2018-03-22 21:11:26 +0100

    Simple camera

--- client/Components/Camera.js
+++ client/Components/Camera.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
-import { Container, P } from '../Styled'
+import { Camera, Permissions } from 'expo'
+import styled from 'styled-components'
 
-const Camera = () => (
-    <Container>
-        <P>Camera</P>
-    </Container>
-)
+class Cameraa extends Component {
+    state = {
+        cameraPermissions: null,
+        type: Camera.Constants.Type.back
+    }
 
-export default Camera
+    async componentWillMount() {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA)
+        this.setState({ cameraPermissions: status === 'granted' })
+    }
+
+    render() {
+        const { cameraPermissions, type } = this.state
+
+        if (!cameraPermissions) return <Container />
+        if (cameraPermissions === false) {
+            return (
+                <Container>
+                    <P>No camera permissions</P>
+                </Container>
+            )
+        }
+        return (
+            <Container>
+                <Camera style={{ flex: 1 }} type={type} />
+            </Container>
+        )
+    }
+}
+
+export default Cameraa
+
+const Container = styled.View`
+    flex: 1;
+`
